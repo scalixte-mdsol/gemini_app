239de0d91dfdbc953a2109729d7c6b64fa956aca
Stanley Calixte <scalixte@mdsol.com>
2015-04-10 16:12:28 -0400

    update content with rails data

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeminiApp::Application.config.secret_token = '8c5dc863ae5045eb39966f4263c0d17abac2fa7b0a28291a0b6b52d7f7a4ae07ccd37efceb484f3d519552b39cd771bb3cc5018268dbf3a5316be2fc1a743f90'
